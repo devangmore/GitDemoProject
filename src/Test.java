39a9cf24cd187b64c6e6da00ec1c23923b326a4d
Devang More <devang2899more@gmail.com>
2023-01-05 16:04:44 -0500

    First commit...

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String active = "yes";
+		if (active == "yes")
+		{
+			System.out.println("Take rest");
+		}
+		else
+		{
+			System.out.println("Work");
+		}
+	}
+}
